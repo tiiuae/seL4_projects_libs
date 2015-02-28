220e85f0d402056c7e72de3524da15a030aacca3
akroh <Alexander.Kroh@nicta.com.au>
2015-02-28 23:28:37 +1100

    Add PLL and MMC clock definitions to access controlled clocks

--- src/devices/exynos/vclock.c
+++ src/devices/exynos/vclock.c
@@ -39,6 +39,40 @@ struct clock_data {
     struct clock_reg* regs;
 };
 
+struct clock_reg sclkmpll_data[] = {
+    CLOCK_REG_DEFN(CORE, 0x000, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(CORE, 0x100, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(CORE, 0x104, 0, 32), /* CON1 */
+};
+struct clock_reg sclkbpll_data[] = {
+    CLOCK_REG_DEFN(CDREX, 0x010, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(CDREX, 0x110, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(CDREX, 0x114, 0, 32), /* CON1 */
+};
+struct clock_reg sclkcpll_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x020, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(TOP, 0x120, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(TOP, 0x124, 0, 32), /* CON1 */
+    CLOCK_REG_DEFN(TOP, 0x128, 0, 32), /* CON2 */
+};
+struct clock_reg sclkgpll_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x050, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(TOP, 0x150, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(TOP, 0x154, 0, 32), /* CON1 */
+};
+struct clock_reg sclkepll_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x030, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(TOP, 0x130, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(TOP, 0x134, 0, 32), /* CON1 */
+    CLOCK_REG_DEFN(TOP, 0x138, 0, 32), /* CON2 */
+};
+struct clock_reg sclkvpll_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x040, 0, 32), /* LOCK */
+    CLOCK_REG_DEFN(TOP, 0x140, 0, 32), /* CON0 */
+    CLOCK_REG_DEFN(TOP, 0x144, 0, 32), /* CON1 */
+    CLOCK_REG_DEFN(TOP, 0x148, 0, 32), /* CON2 */
+};
+
 struct clock_reg uart0_data[] = {
     CLOCK_REG_DEFN(TOP, 0x250, 0, 4), /* SEL  */
     CLOCK_REG_DEFN(TOP, 0x350, 0, 1), /* MASK */
@@ -121,9 +155,51 @@ struct clock_reg i2c7_data[] = {
     CLOCK_REG_DEFN(TOP, 0x950, 13, 1), /* GATE */
 };
 
+struct clock_reg mmc0_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x244, 0, 4), /* SEL */
+    CLOCK_REG_DEFN(TOP, 0x340, 0, 1), /* MASK */
+    CLOCK_REG_DEFN(TOP, 0x54C, 0, 4), /* DIV */
+    CLOCK_REG_DEFN(TOP, 0x54C, 8, 8), /* DIV_PRE */
+    CLOCK_REG_DEFN(TOP, 0x64C, 0, 1), /* DIVSTAT */
+    CLOCK_REG_DEFN(TOP, 0x64C, 8, 1), /* DIVSTAT_PRE */
+    CLOCK_REG_DEFN(TOP, 0x944, 12, 1), /* GATE */
+};
+struct clock_reg mmc1_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x244, 4, 4), /* SEL */
+    CLOCK_REG_DEFN(TOP, 0x340, 4, 1), /* MASK */
+    CLOCK_REG_DEFN(TOP, 0x54C, 16, 4), /* DIV */
+    CLOCK_REG_DEFN(TOP, 0x54C, 24, 8), /* DIV_PRE */
+    CLOCK_REG_DEFN(TOP, 0x64C, 16, 1), /* DIVSTAT */
+    CLOCK_REG_DEFN(TOP, 0x64C, 24, 1), /* DIVSTAT_PRE */
+    CLOCK_REG_DEFN(TOP, 0x944, 13, 1), /* GATE */
+};
+struct clock_reg mmc2_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x244, 8, 4), /* SEL */
+    CLOCK_REG_DEFN(TOP, 0x340, 8, 1), /* MASK */
+    CLOCK_REG_DEFN(TOP, 0x550, 0, 4), /* DIV */
+    CLOCK_REG_DEFN(TOP, 0x550, 8, 8), /* DIV_PRE */
+    CLOCK_REG_DEFN(TOP, 0x650, 0, 1), /* DIVSTAT */
+    CLOCK_REG_DEFN(TOP, 0x650, 8, 1), /* DIVSTAT_PRE */
+    CLOCK_REG_DEFN(TOP, 0x944, 14, 1), /* GATE */
+};
+struct clock_reg mmc3_data[] = {
+    CLOCK_REG_DEFN(TOP, 0x244, 12, 4), /* SEL */
+    CLOCK_REG_DEFN(TOP, 0x340, 12, 1), /* MASK */
+    CLOCK_REG_DEFN(TOP, 0x550, 16, 4), /* DIV */
+    CLOCK_REG_DEFN(TOP, 0x550, 24, 8), /* DIV_PRE */
+    CLOCK_REG_DEFN(TOP, 0x650, 16, 1), /* DIVSTAT */
+    CLOCK_REG_DEFN(TOP, 0x650, 24, 1), /* DIVSTAT_PRE */
+    CLOCK_REG_DEFN(TOP, 0x944, 15, 1), /* GATE */
+};
 
 struct clock_data clock_data[] = {
     [CLK_MASTER  ] = CLK_DEFN_NONE(),
+    [CLK_SCLKMPLL] = CLK_DEFN(sclkmpll_data),
+    [CLK_SCLKBPLL] = CLK_DEFN(sclkbpll_data),
+    [CLK_SCLKCPLL] = CLK_DEFN(sclkcpll_data),
+    [CLK_SCLKGPLL] = CLK_DEFN(sclkgpll_data),
+    [CLK_SCLKEPLL] = CLK_DEFN(sclkepll_data),
+    [CLK_SCLKVPLL] = CLK_DEFN(sclkvpll_data),
     [CLK_SPI0    ] = CLK_DEFN(spi0_data),
     [CLK_SPI1    ] = CLK_DEFN(spi1_data),
     [CLK_SPI2    ] = CLK_DEFN(spi2_data),
@@ -134,12 +210,6 @@ struct clock_data clock_data[] = {
     [CLK_UART2   ] = CLK_DEFN(uart2_data),
     [CLK_UART3   ] = CLK_DEFN(uart3_data),
     [CLK_PWM     ] = CLK_DEFN_NONE(),
-    [CLK_SCLKMPLL] = CLK_DEFN_NONE(),
-    [CLK_SCLKBPLL] = CLK_DEFN_NONE(),
-    [CLK_SCLKCPLL] = CLK_DEFN_NONE(),
-    [CLK_SCLKGPLL] = CLK_DEFN_NONE(),
-    [CLK_SCLKEPLL] = CLK_DEFN_NONE(),
-    [CLK_SCLKVPLL] = CLK_DEFN_NONE(),
     [CLK_I2C0    ] = CLK_DEFN(i2c0_data),
     [CLK_I2C1    ] = CLK_DEFN(i2c1_data),
     [CLK_I2C2    ] = CLK_DEFN(i2c2_data),
@@ -147,7 +217,11 @@ struct clock_data clock_data[] = {
     [CLK_I2C4    ] = CLK_DEFN(i2c4_data),
     [CLK_I2C5    ] = CLK_DEFN(i2c5_data),
     [CLK_I2C6    ] = CLK_DEFN(i2c6_data),
-    [CLK_I2C7    ] = CLK_DEFN(i2c7_data)
+    [CLK_I2C7    ] = CLK_DEFN(i2c7_data),
+    [CLK_MMC0    ] = CLK_DEFN(mmc0_data),
+    [CLK_MMC1    ] = CLK_DEFN(mmc1_data),
+    [CLK_MMC2    ] = CLK_DEFN(mmc2_data),
+    [CLK_MMC3    ] = CLK_DEFN(mmc3_data)
 };
 
 const struct device dev_cmu_cpu = {
@@ -268,11 +342,14 @@ vm_clock_config(struct clock_device* clkd, enum clk_id clk_id, int grant)
 {
     struct clock_data* data = &clock_data[clk_id];
     int i;
+    assert(data);
+    assert(data->nregs);
     for (i = 0; i < data->nregs; i++) {
         uint32_t *mask;
         uint32_t mask_val;
         mask = (uint32_t*)(clkd->mask[data->regs[i].bank] + data->regs[i].offset);
-        mask_val = MASK(data->regs[i].bits) <<  data->regs[i].shift;
+        /* MASK(x) does not handle 32 bit masks */
+        mask_val = ((1UL << data->regs[i].bits) - 1) << data->regs[i].shift;
         if (grant) {
             *mask |= mask_val;
         } else {
