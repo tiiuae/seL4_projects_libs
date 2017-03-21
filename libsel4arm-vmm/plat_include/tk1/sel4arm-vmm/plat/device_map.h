cc713d5065de3d3a8caa9056fd0b22c4fa5029bb
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-03-21 17:34:57 +1100

    libsel4arm-vmm: tk1 add misc devices

--- libsel4arm-vmm/plat_include/tk1/sel4arm-vmm/plat/device_map.h
+++ libsel4arm-vmm/plat_include/tk1/sel4arm-vmm/plat/device_map.h
@@ -16,7 +16,16 @@
 #define RAM_END   0xC0000000
 #define RAM_SIZE (RAM_END - RAM_BASE)
 
-/* a place holder */
-#define ALIVE_PADDR 0x00000000
+#define USB1_PADDR              0x7d000000
+#define USB3_PADDR              0x7d008000
+#define SDMMC_PADDR             0x700b0000
+#define RTC_PADDR               0x7000e000
+#define DATA_MEMORY_PADDR       0x40000000
+#define EXCEPTION_VECTORS_PADDR 0x6000f000
+#define SYSTEM_REGISTERS_PADDR  0x6000c000
+#define ICTLR_PADDR             0x60004000
+#define APB_MISC_PADDR          0x70000000
+#define FUSE_PADDR              0x7000f000
+#define GPIOS_PADDR             0x6000d000
 
 #endif /* SEL4ARM_VMM_TK1_MAP_H */
--- libsel4arm-vmm/src/plat/tk1/devices/misc_devices.c
+++ libsel4arm-vmm/src/plat/tk1/devices/misc_devices.c
@@ -1,9 +1,112 @@
 /*
- * Copyright 2016, NICTA
- *
- * This software may be distributed and modified according to the terms of
- * the BSD 2-Clause license. Note that NO WARRANTY is provided.
- * See "LICENSE_BSD2.txt" for details.
- *
- * @TAG(NICTA_BSD)
- */
+* Copyright 2017, Data61
+* Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+* ABN 41 687 119 230.
+* This software may be distributed and modified according to the terms of
+* the BSD 2-Clause license. Note that NO WARRANTY is provided.
+* See "LICENSE_BSD2.txt" for details.
+* @TAG(D61_BSD)
+*/
+#include <sel4arm-vmm/plat/device_map.h>
+#include <sel4arm-vmm/devices.h>
+
+
+const struct device dev_usb1 = {
+    .devid = DEV_CUSTOM,
+    .name = "usb3",
+    .pstart = USB1_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+
+const struct device dev_usb3 = {
+    .devid = DEV_CUSTOM,
+    .name = "usb3",
+    .pstart = USB3_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_sdmmc = {
+    .devid = DEV_CUSTOM,
+    .name = "usb3",
+    .pstart = SDMMC_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_rtc_kbc_pmc = {
+    .devid = DEV_CUSTOM,
+    .name = "rtc_kbc_pmc",
+    .pstart = RTC_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_data_memory = {
+    .devid = DEV_CUSTOM,
+    .name = "data_memory",
+    .pstart = DATA_MEMORY_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_exception_vectors = {
+    .devid = DEV_CUSTOM,
+    .name = "exception_vectors",
+    .pstart = EXCEPTION_VECTORS_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_system_registers = {
+    .devid = DEV_CUSTOM,
+    .name = "system_registers",
+    .pstart = SYSTEM_REGISTERS_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_ictlr = {
+    .devid = DEV_CUSTOM,
+    .name = "ictlr",
+    .pstart = ICTLR_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_apb_misc = {
+    .devid = DEV_CUSTOM,
+    .name = "apb_misc",
+    .pstart = APB_MISC_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_fuse = {
+    .devid = DEV_CUSTOM,
+    .name = "fuse",
+    .pstart = FUSE_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
+
+const struct device dev_gpios = {
+    .devid = DEV_CUSTOM,
+    .name = "gpios",
+    .pstart = GPIOS_PADDR,
+    .size = 0x1000,
+    .handle_page_fault = NULL,
+    .priv = NULL
+};
