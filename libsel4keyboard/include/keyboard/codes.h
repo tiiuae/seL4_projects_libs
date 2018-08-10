64495242a9b25e3add803d41ec257a3a849a965c
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-08-10 16:38:49 +1000

    sel4keyboard: Added sel4keyboard prefixes
    Prefixed the API functions in libsel4keyboard with 'sel4'.
    Existing applications (e.g. camkes aeroplage) expect the prefix.

--- libsel4keyboard/include/keyboard/codes.h
+++ libsel4keyboard/include/keyboard/codes.h
@@ -15,6 +15,6 @@
 /* Convert a keyboard scan code (from set 2) to its character representation.
  * Returns 0 for anything that is not a scan code for a canonical character.
  */
-char keyboard_code_to_char(int index);
+char sel4keyboard_code_to_char(int index);
 
 #endif
