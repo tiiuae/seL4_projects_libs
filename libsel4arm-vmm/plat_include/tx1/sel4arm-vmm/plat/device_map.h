552d271429ce0e9d3a0db3c8aa3308254ed22b03
Yanyan Shen <yanyan.shen@data61.csiro.au>
2018-12-06 13:15:38 +1100

    sel4arm-vmm: Add TX1 files.

---
+++ libsel4arm-vmm/plat_include/tx1/sel4arm-vmm/plat/device_map.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#ifndef SEL4ARM_VMM_TX1_MAP_H
+#define SEL4ARM_VMM_TX1_MAP_H
+
+/***** Physical Map ****/
+#define RAM_BASE  0x80000000
+#define RAM_END   0x88000000
+#define RAM_SIZE (RAM_END - RAM_BASE)
+
+#endif /* SEL4ARM_VMM_TX1_MAP_H */
---
+++ libsel4arm-vmm/plat_include/tx1/sel4arm-vmm/plat/devices.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#ifndef SEL4ARM_VMM_TX1_DEVICES_H
+#define SEL4ARM_VMM_TX1_DEVICES_H
+
+#include <sel4arm-vmm/plat/device_map.h>
+#include <sel4arm-vmm/vm.h>
+
+#define GIC_PADDR   0x50040000
+#define MAX_VIRQS   200
+extern const struct device dev_vram;
+
+#endif /* SEL4ARM_VMM_TX1_DEVICES_H */
