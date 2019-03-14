6f6250cb4154a83e7c3478021815f220f0e98f4a
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2019-03-14 11:44:49 +1100

    TX2: Add support for guests

---
+++ libsel4arm-vmm/plat_include/tx2/sel4arm-vmm/plat/device_map.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#pragma once
+
+/***** Physical Map ****/
+#define RAM_BASE  0xF1000000
+#define RAM_END   0xF9000000
+#define RAM_SIZE (RAM_END - RAM_BASE)
