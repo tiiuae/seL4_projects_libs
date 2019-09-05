feeb4d32819372a6d43ea1b18dac33207d090125
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2019-09-05 16:45:03 +1000

    libsel4vm: Unhandled arm page faults use callback
    Updated the ARM page fault handling to make use of the
    unhandled mem fault callback when the fault is left unhandled
    after using the reservation interface.

--- libsel4vm/src/arch/arm/mem_abort.c
+++ libsel4vm/src/arch/arm/mem_abort.c
@@ -24,6 +24,40 @@
 #include "vm.h"
 #include "mem_abort.h"
 
+static int unhandled_memory_fault(vm_t *vm, fault_t* fault) {
+    uintptr_t addr = fault_get_address(fault);
+    size_t fault_size = fault_get_width_size(fault);
+    bool is_read_fault = fault_is_read(fault);
+    seL4_Word fault_data = 0;
+
+    if (!is_read_fault) {
+        fault_data = fault_get_data(fault);
+    }
+    memory_fault_result_t fault_result = vm->mem.unhandled_mem_fault_handler(vm, addr, fault_size,
+            fault_is_read(fault), &fault_data, fault_get_data_mask(fault),
+            vm->mem.unhandled_mem_fault_cookie);
+     switch(fault_result) {
+        case FAULT_HANDLED:
+            if (is_read_fault) {
+                fault_set_data(fault, fault_data);
+                advance_fault(fault);
+            }
+            return 0;
+        case FAULT_RESTART:
+            restart_fault(fault);
+            return 0;
+        case FAULT_IGNORE:
+            return ignore_fault(fault);
+        case FAULT_ERROR:
+            print_fault(fault);
+            abandon_fault(fault);
+            return -1;
+        default:
+            break;
+    }
+    return -1;
+}
+
 static inline int dev_paddr_in_range(uintptr_t addr, const struct device* d)
 {
     return ( (addr >= d->pstart) && addr < (d->pstart + d->size) );
@@ -129,6 +163,7 @@ handle_on_demand_fault_callback(vm_t *vm, uintptr_t fault_addr, size_t fault_len
 int
 handle_page_fault(vm_t* vm, fault_t* fault)
 {
+    int err;
     struct device* d;
     guest_memory_arch_data_t arch_data;
     uintptr_t addr = fault_get_address(fault);
@@ -148,7 +183,15 @@ handle_page_fault(vm_t* vm, fault_t* fault)
             print_fault(fault);
             abandon_fault(fault);
             return -1;
-        default: /* FAULT_UNHANDLED */
+        case FAULT_UNHANDLED:
+            if (vm->mem.unhandled_mem_fault_handler) {
+                err = unhandled_memory_fault(vm, fault);
+                if (err) {
+                    return -1;
+                }
+                return 0;
+            }
+        default:
             break;
             /* We don't have a memory reservation for the faulting address
              * We move onto the rest of the page fault handler */
