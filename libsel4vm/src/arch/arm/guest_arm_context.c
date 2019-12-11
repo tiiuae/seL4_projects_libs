064ba8f9474d6b5e8cd2c0863a430e4abf71d0d8
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2019-12-11 12:59:18 +1100

    libsel4vm: arm context interface uses fault state
    Updated the guest arm context interface to account for the
    fault interface if the vcpu is in a middle of a fault. Prior
    this would directly write to the TCB and potentially de-sync
    with the faults state (whom would eventually override anything
    written). If the vcpu is in the middle of a fault we now update
    and read its cached state as this will eventually be syned to the
    TCB when the vcpu resumes.

--- libsel4vm/src/arch/arm/guest_arm_context.c
+++ libsel4vm/src/arch/arm/guest_arm_context.c
@@ -15,54 +15,87 @@
 #include <sel4vm/guest_vm_util.h>
 #include <sel4vm/arch/guest_arm_context.h>
 
+#include "fault.h"
+
 int vm_set_thread_context(vm_vcpu_t *vcpu, seL4_UserContext context) {
-    seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
-    int err = seL4_TCB_WriteRegisters(tcb, false, 0, sizeof(context) / sizeof(context.pc), &context);
-    if (err) {
-        ZF_LOGE("Failed to set thread context: Unable to write TCB registers");
-        return -1;
+    if (!fault_handled(vcpu->vcpu_arch.fault)) {
+        /* If we are in a fault set its context directly as this will be
+         * later synced */
+        fault_set_ctx(vcpu->vcpu_arch.fault, &context);
+    } else {
+        /* Otherwise write to the TCB directly */
+        seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
+        int err = seL4_TCB_WriteRegisters(tcb, false, 0, sizeof(context) / sizeof(context.pc), &context);
+        if (err) {
+            ZF_LOGE("Failed to set thread context: Unable to write TCB registers");
+            return -1;
+        }
     }
     return 0;
 }
 
 int vm_set_thread_context_reg(vm_vcpu_t *vcpu, unsigned int reg, uintptr_t value) {
-    seL4_UserContext regs;
     seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
-    int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
-    if (err) {
-        ZF_LOGE("Failed to set thread context reg: Unable to read TCB registers");
-        return -1;
-    }
-    (&regs.pc)[reg] = value;
-    err = seL4_TCB_WriteRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
-    if (err) {
-        ZF_LOGE("Failed to set thread context register: Unable to write new register to TCB");
-        return -1;
+    if (!fault_handled(vcpu->vcpu_arch.fault)) {
+        /* If we are in a fault use and modify its cached context */
+        seL4_UserContext *regs;
+        regs = fault_get_ctx(vcpu->vcpu_arch.fault);
+        (&regs->pc)[reg] = value;
+    } else {
+        /* Otherwise write to the TCB directly */
+        seL4_UserContext regs;
+        int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
+        if (err) {
+            ZF_LOGE("Failed to set thread context reg: Unable to read TCB registers");
+            return -1;
+        }
+        (&regs.pc)[reg] = value;
+        err = seL4_TCB_WriteRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
+        if (err) {
+            ZF_LOGE("Failed to set thread context register: Unable to write new register to TCB");
+            return -1;
+        }
     }
     return 0;
 }
 
 int vm_get_thread_context(vm_vcpu_t *vcpu, seL4_UserContext *context) {
-    seL4_UserContext regs;
-    seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
-    int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
-    if (err) {
-        ZF_LOGE("Failed to get thread context: Unable to read TCB registers");
-        return -1;
+    if (!fault_handled(vcpu->vcpu_arch.fault)) {
+        /* If we are in a fault use its cached context */
+        seL4_UserContext *regs;
+        regs = fault_get_ctx(vcpu->vcpu_arch.fault);
+        *context = *regs;
+    } else {
+        /* Otherwise read the TCB directly */
+        seL4_UserContext regs;
+        seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
+        int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
+        if (err) {
+            ZF_LOGE("Failed to get thread context: Unable to read TCB registers");
+            return -1;
+        }
+        *context = regs;
     }
-    *context = regs;
     return 0;
 }
 
 int vm_get_thread_context_reg(vm_vcpu_t *vcpu, unsigned int reg, uintptr_t *value) {
-    seL4_UserContext regs;
-    seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
-    int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
-    if (err) {
-        ZF_LOGE("Failed to get thread context register: Unable to read TCB registers");
-        return -1;
+    if (!fault_handled(vcpu->vcpu_arch.fault)) {
+        /* If we are in a fault use its cached context */
+        seL4_UserContext *regs;
+        regs = fault_get_ctx(vcpu->vcpu_arch.fault);
+        *value =  (&regs->pc)[reg];
+    } else {
+        /* Otherwise read it from the TCB directly */
+        seL4_UserContext regs;
+        seL4_CPtr tcb = vm_get_vcpu_tcb(vcpu);
+        int err = seL4_TCB_ReadRegisters(tcb, false, 0, sizeof(regs) / sizeof(regs.pc), &regs);
+        if (err) {
+            ZF_LOGE("Failed to get thread context register: Unable to read TCB registers");
+            return -1;
+        }
+        *value =  (&regs.pc)[reg];
     }
-    *value =  (&regs.pc)[reg];
     return 0;
 }
 
