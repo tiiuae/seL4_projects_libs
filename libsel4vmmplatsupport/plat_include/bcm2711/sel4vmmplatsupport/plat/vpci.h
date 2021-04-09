6794e5860adacd75fd109985325109c4a566ab98
Joonas Onatsu <joonasx@ssrc.tii.ae>
2021-04-09 12:27:06 +0300

    rpi4: Add VM support
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

---
+++ libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022, Technology Innovation Institute
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+/* PCI host bridge configuration space */
+#define PCI_CFG_REGION_ADDR 0x100000000ULL
+/* PCI host bridge IO space */
+#define PCI_IO_REGION_ADDR 0x101000000ULL
+/* Size of PCI configuration space */
+#define PCI_CFG_REGION_SIZE 0x1000000
+/* Size of PCI IO space  */
+#define PCI_IO_REGION_SIZE 0x10000
+/* PCI memory space */
+#define PCI_MEM_REGION_ADDR 0x60000000ULL
+/* PCI memory space size */
+#define PCI_MEM_REGION_SIZE 0x20000000
+
+/* FDT IRQ controller address cells definition */
+#define GIC_ADDRESS_CELLS 0x1
