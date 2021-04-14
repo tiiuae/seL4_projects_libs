aa437fa072ae67c8af39d691e010796e69647841
Joonas Onatsu <joonas.onatsu@unikie.com>
2021-04-14 19:10:00 +0300

    -Test fix GIC address.

--- libsel4vm/src/arch/arm/vgic/vgic.c
+++ libsel4vm/src/arch/arm/vgic/vgic.c
@@ -82,7 +82,7 @@
 #elif defined(CONFIG_PLAT_ODROIDC2)
 #define GIC_PADDR   0xc4300000
 #elif defined(CONFIG_PLAT_BCM2711)
-#define GIC_PADDR   0xff841000
+#define GIC_PADDR   0xff840000
 #else
 #error "Unsupported platform for GIC"
 #endif
