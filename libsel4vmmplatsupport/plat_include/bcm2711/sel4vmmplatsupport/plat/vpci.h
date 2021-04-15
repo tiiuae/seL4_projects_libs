7370df88e0156f182a71f54010054a693a88d98a
Joonas Onatsu <joonas.onatsu@unikie.com>
2021-04-15 17:39:40 +0300

    -Updated VPCI addresses for RPi4.

--- libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
+++ libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
@@ -9,19 +9,20 @@
  * supplied to the Linux guest. These values are also reflected here.
  */
 
+// Mapped at 0x100000000, size 0x3000000 (48MB)
 
 /* PCI host bridge configration space */
-#define PCI_CFG_REGION_ADDR 0xFD000000 //0x3E000000
+#define PCI_CFG_REGION_ADDR 0x100000000 //0x3E000000
 /* PCI host bridge IO space */
-#define PCI_IO_REGION_ADDR 0xFC000000 // 0x3D000000
+#define PCI_IO_REGION_ADDR 0x101000000  // 0x3D000000
 /* Size of PCI configuration space */
-#define PCI_CFG_REGION_SIZE 0x1000000
+#define PCI_CFG_REGION_SIZE 0x1000000   // 16MB
 /* Size of PCI IO space  */
-#define PCI_IO_REGION_SIZE 0x10000
+#define PCI_IO_REGION_SIZE 0x10000      // 64kB
 /* PCI memory space */
-#define PCI_MEM_REGION_ADDR 0xFE000000ull // 0x3F000000ull
+#define PCI_MEM_REGION_ADDR 0x102000000ull // 0x3F000000ull
 /* PCI memory space size */
-#define PCI_MEM_REGION_SIZE 0x1000000
+#define PCI_MEM_REGION_SIZE 0x1000000   // 16MB
 
 /* FDT IRQ controller address cells definition */
 #define GIC_ADDRESS_CELLS 0x2
