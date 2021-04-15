c83da56ba6aaa3b9196a14764091ca49da24c0be
Joonas Onatsu <joonas.onatsu@unikie.com>
2021-04-15 14:38:49 +0300

    -Updated VPCI addresses for RPi4.

--- libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
+++ libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
@@ -9,22 +9,17 @@
  * supplied to the Linux guest. These values are also reflected here.
  */
 
-/*
- * NOTE!!! These values are probably not correct,
- *         as this file is just copied from another
- *         platform! TODO: fix/check values correct for BCM2711
- */
 
 /* PCI host bridge configration space */
-#define PCI_CFG_REGION_ADDR 0x3E000000
+#define PCI_CFG_REGION_ADDR 0xFD000000 //0x3E000000
 /* PCI host bridge IO space */
-#define PCI_IO_REGION_ADDR 0x3D000000
+#define PCI_IO_REGION_ADDR 0xFC000000 // 0x3D000000
 /* Size of PCI configuration space */
 #define PCI_CFG_REGION_SIZE 0x1000000
 /* Size of PCI IO space  */
 #define PCI_IO_REGION_SIZE 0x10000
 /* PCI memory space */
-#define PCI_MEM_REGION_ADDR 0x3F000000ull
+#define PCI_MEM_REGION_ADDR 0xFE000000ull // 0x3F000000ull
 /* PCI memory space size */
 #define PCI_MEM_REGION_SIZE 0x1000000
 
