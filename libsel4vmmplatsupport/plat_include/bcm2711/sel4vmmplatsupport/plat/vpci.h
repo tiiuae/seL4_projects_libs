be1f5f96608a17dd10605ce828912ddcffdb9dd1
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2021-04-21 13:47:29 +0300

    Use 32 bit address space for PCI, fix GIC cell size
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

--- libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
+++ libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
@@ -9,20 +9,28 @@
  * supplied to the Linux guest. These values are also reflected here.
  */
 
-// Mapped at 0x100000000, size 0x3000000 (48MB)
+// Mapped at 0xf0000000, size 0x3000000 (48MB)
+//
+// Note that the mapping address must be inside 32-bit address space,
+// since on guest Linux the PCI resource does not have IORESOURCE_MEM_64
+// set (see pci_bus_alloc_resource in drivers/pci/bus.c) and therefore
+// it tries to use 32-bit address space only.  TODO: find out whether
+// this is something that can be configured in the guest, or does the
+// BAR registers and/or VMM's emulated PCI device have something to do
+// with it.
 
 /* PCI host bridge configration space */
-#define PCI_CFG_REGION_ADDR 0x100000000 //0x3E000000
+#define PCI_CFG_REGION_ADDR 0xf0000000
 /* PCI host bridge IO space */
-#define PCI_IO_REGION_ADDR 0x101000000  // 0x3D000000
+#define PCI_IO_REGION_ADDR 0xf1000000
 /* Size of PCI configuration space */
 #define PCI_CFG_REGION_SIZE 0x1000000   // 16MB
 /* Size of PCI IO space  */
 #define PCI_IO_REGION_SIZE 0x10000      // 64kB
 /* PCI memory space */
-#define PCI_MEM_REGION_ADDR 0x102000000ull // 0x3F000000ull
+#define PCI_MEM_REGION_ADDR 0xf2000000ull
 /* PCI memory space size */
 #define PCI_MEM_REGION_SIZE 0x1000000   // 16MB
 
 /* FDT IRQ controller address cells definition */
-#define GIC_ADDRESS_CELLS 0x2
+#define GIC_ADDRESS_CELLS 0x1
