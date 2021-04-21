7205b7fc5e11f2a11a77579e265c4cd52bf5678d
Hannu Lyytinen <hannux@ssrc.tii.ae>
2021-04-21 13:47:29 +0300

    HYPR-161 Enlarge PCI memory window
    Since the virtio PoC exposes all of the user-VM's memory to the
    driver-VM via a CAmkES dataport, the PCI memory window must be
    large enough to accommodate the guest memory. Right now we have
    128 MB configured for the user-VM, but to be on the safe side,
    lets use 512 MB here.
    
    The address 0x50000000 is picked because the driver-VM currently
    occupies 0x40000000 - 0x47ffffff and the user-VM occupies
    0x48000000 - 0x4fffffff. In other words, nothing is there
    currently. One would think this address is specific to the
    vspace of the VM and hence it is enough if the current VM does
    not have any other kind of memory -- but because the PCI memory
    window is statically configured in the header and is the same for
    every VM, we need to compute a union of all used address ranges
    and then select some range outside of this union. Perhaps some
    day we could have per-VM defined PCI memory window?
    
    It appears that the PCI memory window shall be within the 32-bit
    address space until we introduce 64-bit addressing to seL4's
    virtual PCI support.
    
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

--- libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
+++ libsel4vmmplatsupport/plat_include/bcm2711/sel4vmmplatsupport/plat/vpci.h
@@ -9,7 +9,6 @@
  * supplied to the Linux guest. These values are also reflected here.
  */
 
-//
 // Note that the mapping address must be inside 32-bit address space,
 // since on guest Linux the PCI resource does not have IORESOURCE_MEM_64
 // set (see pci_bus_alloc_resource in drivers/pci/bus.c) and therefore
@@ -19,17 +18,17 @@
 // with it.
 
 /* PCI host bridge configration space */
-#define PCI_CFG_REGION_ADDR 0x100000000 // 0x600000000 // 0xf0000000
+#define PCI_CFG_REGION_ADDR 0x100000000ULL //0x3E000000
 /* PCI host bridge IO space */
-#define PCI_IO_REGION_ADDR 0x101000000 // 0x601000000 // 0xf1000000
+#define PCI_IO_REGION_ADDR 0x101000000ULL  // 0x3D000000
 /* Size of PCI configuration space */
 #define PCI_CFG_REGION_SIZE 0x1000000   // 16MB
 /* Size of PCI IO space  */
 #define PCI_IO_REGION_SIZE 0x10000      // 64kB
 /* PCI memory space */
-#define PCI_MEM_REGION_ADDR 0x02000000 // 0x602000000ull // 0xf2000000ull
+#define PCI_MEM_REGION_ADDR 0x50000000ULL
 /* PCI memory space size */
-#define PCI_MEM_REGION_SIZE 0x1000000   // 16MB
+#define PCI_MEM_REGION_SIZE 0x20000000   // 512 MB
 
 /* FDT IRQ controller address cells definition */
 #define GIC_ADDRESS_CELLS 0x1
