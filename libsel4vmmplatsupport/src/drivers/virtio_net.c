1243e6f68c4d2b12a63eb1628e64acfb57097f97
Hannu Lyytinen <hannux@ssrc.tii.ae>
2022-02-06 21:29:50 +0200

    libsel4vmmplatsupport: clean up PCI emulation API
    ARM and x86 behave the same way now, no need to pass along any flag
    whether or not to emulate BARs.
    
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- libsel4vmmplatsupport/src/drivers/virtio_net.c
+++ libsel4vmmplatsupport/src/drivers/virtio_net.c
@@ -123,7 +123,7 @@ struct raw_iface_funcs virtio_net_default_backend()
 
 static vmm_pci_entry_t vmm_virtio_net_pci_bar(unsigned int iobase,
                                               size_t iobase_size_bits, unsigned int interrupt_pin,
-                                              unsigned int interrupt_line, bool emulate_bar_access)
+                                              unsigned int interrupt_line)
 {
     vmm_pci_device_def_t *pci_config;
     int err = ps_calloc(&ops.malloc_ops, 1, sizeof(*pci_config), (void **)&pci_config);
@@ -161,7 +161,7 @@ static vmm_pci_entry_t vmm_virtio_net_pci_bar(unsigned int iobase,
 
 virtio_net_t *common_make_virtio_net(vm_t *vm, vmm_pci_space_t *pci, vmm_io_port_list_t *ioport,
                                      ioport_range_t ioport_range, ioport_type_t port_type, unsigned int interrupt_pin, unsigned int interrupt_line,
-                                     struct raw_iface_funcs backend, bool emulate_bar_access)
+                                     struct raw_iface_funcs backend)
 {
     int err = ps_new_stdlib_malloc_ops(&ops.malloc_ops);
     ZF_LOGF_IF(err, "Failed to get malloc ops");
@@ -180,8 +180,7 @@ virtio_net_t *common_make_virtio_net(vm_t *vm, vmm_pci_space_t *pci, vmm_io_port
     size_t iobase_size_bits = BYTES_TO_SIZE_BITS(io_entry->range.size);
     net->iobase = io_entry->range.start;
 
-    vmm_pci_entry_t entry = vmm_virtio_net_pci_bar(io_entry->range.start, iobase_size_bits, interrupt_pin, interrupt_line,
-                                                   emulate_bar_access);
+    vmm_pci_entry_t entry = vmm_virtio_net_pci_bar(io_entry->range.start, iobase_size_bits, interrupt_pin, interrupt_line);
     vmm_pci_add_entry(pci, entry, NULL);
 
     ps_io_ops_t ioops;
