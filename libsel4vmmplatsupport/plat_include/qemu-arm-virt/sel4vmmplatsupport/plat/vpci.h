0bb4de51ae81b6d5cc75a3cd268dab1612eec9f7
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2024-01-09 16:22:04 +0200

    qemu-arm-virt: increase vPCI MMIO region
    Change vPCI layout in order to accommodate larger MMIO regions.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- libsel4vmmplatsupport/plat_include/qemu-arm-virt/sel4vmmplatsupport/plat/vpci.h
+++ libsel4vmmplatsupport/plat_include/qemu-arm-virt/sel4vmmplatsupport/plat/vpci.h
@@ -10,17 +10,17 @@
  */
 
 /* PCI host bridge configration space */
-#define PCI_CFG_REGION_ADDR 0xDE000000
+#define PCI_CFG_REGION_ADDR 0xE0000000
 /* PCI host bridge IO space */
-#define PCI_IO_REGION_ADDR 0xDD000000
+#define PCI_IO_REGION_ADDR 0xE1000000
 /* Size of PCI configuration space */
 #define PCI_CFG_REGION_SIZE 0x1000000
 /* Size of PCI IO space  */
 #define PCI_IO_REGION_SIZE 0x10000
 /* PCI memory space */
-#define PCI_MEM_REGION_ADDR 0xDF000000ull
+#define PCI_MEM_REGION_ADDR 0xC0000000ULL
 /* PCI memory space size */
-#define PCI_MEM_REGION_SIZE 0x1000000
+#define PCI_MEM_REGION_SIZE 0x20000000
 
 /* FDT IRQ controller address cells definition */
 #define GIC_ADDRESS_CELLS 0x2
